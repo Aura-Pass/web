10e6ddff69009969a4023cd38d6be7b15728d9e2
João Túlio <joaotuliosoares@hotmail.com>
2024-05-20 23:48:42 -0300

    chore: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import "../src/app/globals.css";
+import type { Preview } from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "dark",
+    attributeName: "data-mode",
+  }),
+  withThemeByClassName({
+    themes: {
+      light: "",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export function Main() {
+    return (
+        <section className="flex flex-col items-center justify-center gap-5 text-4xl text-white">
+            <Image src={"/logos/aura_pass.svg"} width={500} height={30} alt="" />
+            <h2>Aura Pass</h2>
+        </section>
+    )
+}
