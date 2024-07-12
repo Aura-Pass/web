331fe90c936bb4b9f7fe694123647eb97c4cce49
joaotuliojt <joaotuliosoares@hotmail.com>
2024-07-12 10:34:30 -0300

    feat: add open graph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,51 @@ const redHat = Red_Hat_Display({
 
 export const metadata: Metadata = {
   title: 'Aura Pass',
-  description: 'Aura Pass Aplication',
+  description:
+    'O Aura Pass é a plataforma online de eventos mais modernizada do Brasil. Shows, festas, teatros, stand up, cursos e muito mais. Encontre e organize eventos com o Aura Pass!',
   icons: '/favicon.svg',
+  keywords: [
+    'eventos',
+    'ingressos',
+    'shows',
+    'concertos',
+    'teatro',
+    'palestras',
+    'workshops',
+    'conferências',
+    'seminários',
+    'cursos',
+    'feiras',
+    'festas',
+    'eventos corporativos',
+    'eventos online',
+    'eventos presenciais',
+    'bilhetes',
+    'venda de ingressos',
+    'plataforma de eventos',
+    'gestão de eventos',
+    'organização de eventos',
+    'inscrições',
+    'eventos culturais',
+    'eventos esportivos',
+    'eventos musicais',
+    'ingressos online',
+    'tickets',
+    'compra de ingressos',
+    'reserva de ingressos',
+    'agenda de eventos',
+    'promoção de eventos',
+    'Aura Pass',
+    'Aura',
+  ],
+  openGraph: {
+    locale: 'pt_BR',
+    type: 'website',
+    url: 'https://aurapass.com.br',
+    title: 'Aura Pass',
+    description:
+      'Os melhores eventos e conteúdos online estão no Aura Pass. Teatros, festas, shows, eventos corporativos, gastronômicos e muito mais. Garanta seu ingresso!',
+  },
 };
 
 export default function RootLayout({
